b8b673f7e5f1e2793e04413b50ef0e777c83416b
arjunyuvaraj <arjunycoding@gmail.com>
2021-05-02 12:30:27 -0400

    adding a AUTO SUBMITTING FUNCTION (woo hoo woo hoo)

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
-let submit = document.getElementById("submit")
+let submit = $("#submit")
+let error = $(".error")
+error.hide()
 let info = {
     "vidya":"123"
 }
+function storeLgin(){
+    let uname = $("#uname").val()
+    let pwd = $("#pwd").val()
+    localStorage.setItem('username', uname);
+    localStorage.setItem('passowrd', pwd);
+}
+function autoFillLogin(){
+    let uname = $("#uname")
+    let pwd = $("#pwd")
+    uname.val(localStorage.getItem('username'))
+    pwd.val(localStorage.getItem('passowrd'))
+}
+autoFillLogin()
 function validateForm(){
-    let uname = document.getElementById("uname").value
-    let pwd = document.getElementById("pwd").value
+    let uname = $("#uname").val()
+    let pwd = $("#pwd").val()
     console.log(`Username :: ${uname}`)
     console.log(`Password :: ${pwd}`)
     for (const [key, value] of Object.entries(info)) {
         console.log(key + value)
         if(key == uname && value == pwd){
-            window.location = "main.html"
+            storeLgin()
+            console.log('Sucsees')
         } else {
-            console.log("failed")
+            error.show()
         }
     }
 }
-// const test = {a: 1, b: 2, c: 3};
-submit.addEventListener("click", validateForm)
+function autoSubmit(){
+    let uname = $("#uname").val()
+let pwd = $("#pwd").val()
+    if(uname == localStorage.getItem('username') && pwd == localStorage.getItem('passowrd')){
+        validateForm()
+    } else {
+        return
+    }
+}
+autoSubmit()
+// let uname = $("#uname").val()
+// let pwd = $("#pwd").val()
+// // console.log(uname == localStorage.getItem('username') && pwd == localStorage.getItem('passowrd'))
+// console.log(`LOCAL SOTRAGE :: username == ${localStorage.getItem('username')}|| password == ${localStorage.getItem('passowrd')}`)
+// console.log(uname + pwd )
+submit.on("click", validateForm)
