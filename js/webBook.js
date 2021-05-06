97af7765b0654e9fd2bebd016f553beb6252dfc3
arjunyuvaraj <arjunycoding@gmail.com>
2021-05-06 19:30:43 -0400

    adding a badge revliling system

--- js/webBook.js
+++ js/webBook.js
@@ -1,6 +1,23 @@
-let book1 = $("#book1");
-function showContent(){
-    console.log("hiiii")
+let badge1 = $("#badge1");
+let unlock = $("#unlock");
+let proflieBtn = $("#proflieBtn");
+let proflie = $("#porfile");
+let page = $("#page");
+let homeBtn = $("#homeBtn");
+proflie.hide()
+badge1.hide()
+function showBadge(){
+    badge1.show()
+    unlock.hide()
 }
-book1.on("click", showContent())
-let story = $("#story");
+function showProflie(){
+    proflie.show()
+    page.hide()
+}
+function showHome(){
+    proflie.hide()
+    page.show()
+}
+unlock.on("click", showBadge)
+proflieBtn.on("click", showProflie)
+homeBtn.on("click", showHome)
